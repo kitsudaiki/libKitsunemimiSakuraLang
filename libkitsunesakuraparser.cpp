e1afb64392c9e779330c400978bf33ecafed9260
Tobias Anker <tobias.anker@kitsunemimi.moe>
2018-12-10 20:48:14 +0100

    initial commit

---
+++ libkitsunesakuraparser.cpp
@@ -0,0 +1,6 @@
+#include "libkitsunesakuraparser.h"
+
+
+LibKitsuneSakuraParser::LibKitsuneSakuraParser()
+{
+}
---
+++ libkitsunesakuraparser.h
@@ -0,0 +1,12 @@
+#ifndef LIBKITSUNESAKURAPARSER_H
+#define LIBKITSUNESAKURAPARSER_H
+
+
+class LibKitsuneSakuraParser
+{
+
+public:
+    LibKitsuneSakuraParser();
+};
+
+#endif // LIBKITSUNESAKURAPARSER_H
